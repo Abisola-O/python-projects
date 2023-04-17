e9695134f3ba5eb0d43df0627119eba0ba737339
Abisola Olayinka <abisola86@outlook.com>
2023-04-17 21:12:35 +0100

    add guessing game v2

---
+++ guessing-game/guessing-game2.py
@@ -0,0 +1,23 @@
+print("Welcome to the guessing game!")
+
+import random
+
+random_number = random.randint(1,10)
+guess = None
+
+while True:
+	guess = input("Pick a number from 1 to 10: ")
+	guess = int(guess)
+	if guess < random_number:
+		print("TOO LOW! TRY AGAIN.")
+	elif guess > random_number:
+		print("TOO HIGH! TRY AGAIN")
+	else:
+		print("YOU GUESSED CORRECTLY!!!!")
+		play_again = input("Do you want to play again? (y/n) ")
+		if play_again == "y":
+			random_number = random.randint(1,10)  
+			guess = None
+		else:
+			print("Thank you for playing!")
+			break
